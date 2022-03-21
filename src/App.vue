8c4f1ba2cf368ff64c9b86666c721d6496188685
Your Name~ <Danilofossatto2011@hotmail.com>
2022-03-21 11:27:11 -0300

    final

--- src/App.vue
+++ src/App.vue
@@ -6,37 +6,60 @@ import Pokemon from './components/Pokemon.vue'
   components: { Pokemon },
    name: 'App',
    data(){
-     pokemons: []
-   },
+    return {pokemons: [] ,
+     search:''
+  }},
    created: function(){
      axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res =>{
        this.pokemons = res.data.results
-       
+       console.log(res.data.results)
      })
    },
+   computed:{
+     searchResult: function(){
+       if(this.search == '' || this.search == ' '){
+         return this.pokemons
+       }else{
+         return this.pokemons.filter(pokemon => pokemon.name == this.search)
+       }
+     }
+   }
    
  }
 </script>
 
 <template>
   <div id="app">
-    <img class="pokeLogo" src="./assets/Poke.png" alt="" srcset="">
+    <div class="Top">
+        <img class="pokeLogo" src="./assets/Poke.png" alt="" srcset="">
+    <input v-model="search" id="Search" type="text" placeholder="Buscar Pokemon" class="input is-rounded "/>
+    </div>
+  
     <div class="columns is-desktop is-multiline ">
-    <div v-for="(poke,index) in pokemons" :key="index">
-      <Pokemon  class="column  " :name="poke.name.charAt(0).toUpperCase() + poke.name.slice(1)" :url="poke.url" :num="index+1" />
+    <div v-for="(poke,index) in searchResult" :key="index">
+       <Pokemon  class="column  " :name="poke.name.charAt(0).toUpperCase() + poke.name.slice(1)" :url="poke.url" :num="index+1"/>
     </div>
     </div>
     </div>
 </template>
 
 <style>
+body{
+  height: 100%;
+  background-color: #1c2733;
+}
 .columns{
  justify-content: center;
 }
+.Top{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
 .pokeLogo{
   margin-top: 20px;
-  margin-bottom: 100px;
-  width: 500px;
+  width: 400px;
 }
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -45,7 +68,13 @@ import Pokemon from './components/Pokemon.vue'
   text-align: center;
   color: #2c3e50;
   max-width: 3000px;
-  
+  background-color: #1c2733;
+  height: 6000px;
 }
+#Search{
+  width: 1000px;
+  margin-top:30px;
+}
+
 </style>
   
